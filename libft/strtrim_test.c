b8a32b144b0349048345f9fd7e903ed4a8c7d3cc
Wonjin <wonjlee@student.42seoul.kr>
2020-12-28 16:20:56 +0900

    part2

--- libft/strtrim_test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-char *ft_strtrim(char const *s1, char const *set);
-
-int	main()
-{
-	printf("%s", ft_strtrim("abcdef", ""));
-}
--- libft/substr_test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-
-int	main(void)
-{
-	printf("%s\n", ft_substr("abcdefg", 8, 0));
-}
